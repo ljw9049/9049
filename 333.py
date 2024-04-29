27fb7ed69a55c141e3da3c42e39a26543ed9336c
ljw9049 <125532097+ljw9049@users.noreply.github.com>
2024-04-29 17:24:17 +0900

    Update 333.py

--- 333.py
+++ 333.py
@@ -26,8 +26,8 @@ worksheet.set_column('L:L', 15)
 worksheet.set_column('M:M', 15)
 
 worksheet.write(0, 0, 'Name', format)
-worksheet.write(0, 1, 'Serial', format)
-worksheet.write(0, 2, 'PID', format)
+worksheet.write(0, 1, 'SN', format)
+worksheet.write(0, 2, 'Pid', format)
 
 
 row = 1
