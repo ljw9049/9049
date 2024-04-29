0f5b86e15c74c058949e452e9ecdeafb9e77554d
ljw9049 <125532097+ljw9049@users.noreply.github.com>
2024-04-29 14:57:46 +0900

    Create 333.py

---
+++ 333.py
@@ -0,0 +1,53 @@
+import xlsxwriter
+
+file = open("/var/lib/awx/projects/_11__ljw90/Result/cisco.log", 'r')
+data = file.readlines()
+workbook = xlsxwriter.Workbook('/var/lib/awx/projects/_11__ljw90/cisco.xlsx')
+worksheet = workbook.add_worksheet('Result')
+format = workbook.add_format({'bold': True, 'align': 'center'})
+format.set_border()
+format.set_pattern(1)
+format.set_bg_color('#A9BCF5')
+
+format2 = workbook.add_format()
+format2.set_border()
+worksheet.set_column('A:A', 20)
+worksheet.set_column('B:B', 15)
+worksheet.set_column('C:C', 15)
+worksheet.set_column('D:D', 15)
+worksheet.set_column('E:E', 15)
+worksheet.set_column('F:F', 15)
+worksheet.set_column('G:G', 15)
+worksheet.set_column('H:H', 15)
+worksheet.set_column('I:I', 15)
+worksheet.set_column('J:J', 15)
+worksheet.set_column('K:K', 15)
+worksheet.set_column('L:L', 15)
+worksheet.set_column('M:M', 15)
+
+worksheet.write(0, 0, 'Hostname', format)
+worksheet.write(0, 1, 'Serial', format)
+worksheet.write(0, 2, 'Uptime', format)
+worksheet.write(0, 3, 'OS Ver', format)
+worksheet.write(0, 4, 'CPU', format)
+worksheet.write(0, 5, 'Memory', format)
+worksheet.write(0, 6, 'Fan', format)
+worksheet.write(0, 7, 'Temp', format)
+worksheet.write(0, 8, 'Power', format)
+worksheet.write(0, 9, 'INT Error-disable', format)
+worksheet.write(0, 10, 'INT CRC Error', format)
+
+row = 1
+col = 0
+
+for i in data:
+    for j in i.split('|'):
+        worksheet.write(row, col, j, format2)
+        col += 1
+    col = 0
+    row += 1
+
+workbook.close()
+
+
+
