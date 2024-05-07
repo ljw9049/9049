48ddf8b7a1f8e3c43c781c9ba1d1df52e863ba61
ljw9049 <125532097+ljw9049@users.noreply.github.com>
2024-05-07 11:31:20 +0900

    Update 555.py

--- 555.py
+++ 555.py
@@ -16,6 +16,12 @@ format.set_bg_color('#A9BCF5')
 
 row = 1
 col = 0
+worksheet.set_column('A:A', 20)
+worksheet.set_column('B:B', 15)
+worksheet.set_column('C:C', 15)
+worksheet.set_column('D:D', 15)
+worksheet.set_column('E:E', 15)
+worksheet.set_column('F:F', 15)
 worksheet.write(0, 0, 'Hostname', format)
 worksheet.write(0, 1, 'NAME', format)
 worksheet.write(0, 2, 'DESCR', format)
